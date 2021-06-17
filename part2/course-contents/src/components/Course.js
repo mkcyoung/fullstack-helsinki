cc33a2c06175bbc07b4c4feeebe244e2d2ceb8bf
mkcyoung <m.c.young0@gmail.com>
2021-06-17 10:36:10 -0600

    part2 exercises 2.1 to 2.5

---
+++ part2/course-contents/src/components/Course.js
@@ -0,0 +1,44 @@
+import React from 'react'
+
+const Header = ({ course }) => {
+    return (
+      <h1>{course.name}</h1>
+    )
+  }
+  
+  const Total = ({ course }) => {
+    const sum = course.parts.reduce((sum,part) => {
+      return sum + part.exercises
+    }, 0)
+    return(
+      <p><b>total of {sum} exercises</b></p>
+    ) 
+  }
+  
+  const Part = (props) => {
+    return (
+      <p>
+        {props.part.name} {props.part.exercises}
+      </p>    
+    )
+  }
+  
+  const Content = ({ course }) => {
+    return (
+      <div>
+        {course.parts.map((x) => <Part key={x.id} part={x}/>)}
+      </div>
+    )
+  }
+  
+  const Course = ({course}) => {
+    return (
+      <div>
+        <Header course={course} />
+        <Content course={course} />
+        <Total course={course} />
+      </div>
+    )
+  }
+
+  export default Course
