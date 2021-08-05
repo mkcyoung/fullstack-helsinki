9a95f3bb203ed986aec69c9ed20cf692201f0441
mkcyoung <m.c.young0@gmail.com>
2021-08-05 08:53:50 -0600

    exercise 4.5

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -4,7 +4,22 @@ const dummy = (blogs) => {
 
 const totalLikes = (blogs) => blogs.reduce((sum, blog) => sum + blog.likes, 0)
 
+const favoriteBlog = (blogs) => {
+    if (blogs.length === 0){
+        return null
+    }
+    return blogs.reduce((favorite,blog) => {
+        if (blog.likes > favorite.likes){
+            return { title: blog.title, author: blog.author, likes: blog.likes }
+        }
+        else {
+            return {title: favorite.title, author: favorite.author, likes: favorite.likes }
+        }
+    }, blogs[0])
+}
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
