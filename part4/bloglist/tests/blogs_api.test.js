fa68db3fd6731e62e2448651c090c009ccc16c17
mkcyoung <m.c.young0@gmail.com>
2021-08-05 12:00:59 -0600

    exercise 4.8

---
+++ part4/bloglist/tests/blogs_api.test.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const helper = require('./test_helper')
+const app = require('../app')
+const Blog = require('../models/blog')
+const api = supertest(app)
+
+beforeEach(async () => {
+    await Blog.deleteMany({})
+
+    const blogObjects = helper.initBlogs
+        .map(blog => new Blog(blog))
+    const promiseArray = blogObjects.map(blog => blog.save())
+    await Promise.all(promiseArray)
+})
+
+
+test('get request returns correct amount of blogs in json format', async () => {
+    const response = await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+
+    expect(response.body).toHaveLength(helper.initBlogs.length)
+  })
+  
+  afterAll(() => {
+    mongoose.connection.close()
+  })
