90bc96add016d91595eefed9f12e8d3510d82f24
mkcyoung <m.c.young0@gmail.com>
2021-08-06 12:18:48 -0600

    exercise 4.21

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -36,8 +36,20 @@ blogsRouter.post('/', async (request, response, next) => {
 })
 
 blogsRouter.delete('/:id', async (request,response,next) => {
-    await Blog.findByIdAndRemove(request.params.id)
-    response.status(204).end()
+    const token = request.token
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+    if (!token || !decodedToken.id) {
+        return response.status(401).json({ error: 'token missing or invalid' })
+    }
+    const user = await User.findById(decodedToken.id)
+    const blog = await Blog.findById(request.params.id)
+    if ( blog.user.toString() === user._id.toString() ){
+        blog.remove()
+        response.status(204).end()
+    }
+
+    // await Blog.findByIdAndRemove(request.params.id)
+    // response.status(204).end()
 })
 
 blogsRouter.put('/:id', async (request, response, next) => {
