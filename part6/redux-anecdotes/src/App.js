57aedc0fdffa1f001b89bab5d8101513789ad5fb
mkcyoung <m.c.young0@gmail.com>
2021-08-13 16:15:17 -0600

    exercise 6.19

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -2,6 +2,7 @@ import React, {useEffect} from 'react'
 import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
 import Notification from './components/Notification'
+import ConnectedNotification from './components/Notification-connect'
 import Filter from './components/Filter'
 import { initializeAnecdotes } from './reducers/anecdoteReducer'
 import { useDispatch } from 'react-redux'
@@ -16,7 +17,8 @@ const App = () => {
   return (
     <div>
       <h2>Anecdotes</h2>
-      <Notification />
+      {/* <Notification /> */}
+      <ConnectedNotification />
       <Filter />
       <AnecdoteList />
       <AnecdoteForm />
---
+++ part6/redux-anecdotes/src/components/Notification-connect.js
@@ -0,0 +1,30 @@
+
+import React from 'react'
+import { connect } from 'react-redux'
+import { useSelector } from 'react-redux'
+
+const Notification = (props) => {
+  const notification = props.message //useSelector(state => state.notification.message)
+  const display = props.display //useSelector(state => state.notification.display)
+  const style = {
+    border: 'solid',
+    padding: 10,
+    borderWidth: 1,
+    display
+  }
+  return (
+    <div style={style}>
+      {notification}
+    </div>
+  )
+}
+
+const mapStateToProps = (state) => {
+  return {
+    message: state.notification.message,
+    display: state.notification.display
+  }
+}
+
+const ConnectedNotification = connect(mapStateToProps)(Notification)
+export default ConnectedNotification
