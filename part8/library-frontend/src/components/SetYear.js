5d9215896d526b1b859cbba6af1ef41b016c057e
mkcyoung <m.c.young0@gmail.com>
2021-08-26 11:06:52 -0600

    exercise 8.12

--- part8/library-frontend/src/components/SetYear.js
+++ part8/library-frontend/src/components/SetYear.js
@@ -1,36 +1,45 @@
-import React, { useState } from 'react'
-import { useMutation } from '@apollo/client';  
+import React, { useState, useRef } from 'react'
+import { useMutation, useQuery } from '@apollo/client';  
 import { ALL_AUTHORS, SET_BIRTH_YEAR } from '../queries'
-
+import Select from 'react-select';
 
 const SetYear = (props) => {
 
-    const [author, setAuthor] = useState('')
+    const [author, setAuthor] = useState(null)
     const [year, setYear] = useState('')
+
+    const selectInputRef = useRef();
+
+    const result = useQuery(ALL_AUTHORS) // may need some async handling or something with this?
 
     const [ setBirthYear ] = useMutation(SET_BIRTH_YEAR, {
         refetchQueries: [ { query: ALL_AUTHORS } ]
       })
 
+    if (result.loading) {
+        return <div> loading ...</div>
+    }
+    const allAuthors = result.data.allAuthors.map(a=> { return { 'value': a.name, 'label':a.name } })
+
     const submit = async (event) => {
         event.preventDefault()
         
-        setBirthYear({  variables: { author, year } })
+        setBirthYear({  variables: { author: author.value, year } })
 
+        setAuthor(null)
         setYear('')
-        setAuthor('')
+        selectInputRef.current.select.clearValue();
     }
 
     return (
         <div>
             <form onSubmit={submit}>
-            <div>
-                name
-                <input
-                value={author}
-                onChange={({ target }) => setAuthor(target.value)}
-                />
-            </div>
+            <Select
+                ref={selectInputRef}
+                defaultValue={author}
+                onChange={setAuthor}
+                options={allAuthors}
+            />
             <div>
                 born
                 <input
